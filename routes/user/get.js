821df20d076a924ef0ddb1616f0fda5f6287f198
Tyler van der Hoeven <hi@tyvdh.com>
2018-05-30 23:02:40 -0400

    much better auth flow

--- routes/user/get.js
+++ routes/user/get.js
@@ -1,60 +1,58 @@
 import moment from 'moment'
 import speakeasy from 'speakeasy'
 import QRCode from 'qrcode'
-import { generateKeyPair, encode32 } from '../../js/stellar'
-import { getJwt, setJwt } from '../../js/jwt';
-import { encrypt, decrypt } from '../../js/crypto'
+import { getStellarServer, generateKeyPair } from '../../js/stellar'
+import { setJwt } from '../../js/jwt';
+import { encrypt } from '../../js/crypto'
+
+import crypto from 'crypto'
+
+// TODO: Once publicKey has been displayed never again show qrCode & secret
 
 export default async function(req, res, next) {
   const secrets = req.webtaskContext.secrets
-  const token = req.headers['authorization'] ? req.headers['authorization'].split(' ')[1] : null
+  const secret = encrypt(req.query.id + secrets.TOTP_SECRET)
+  const keyPair = generateKeyPair(req.url, secret + secrets.STR_SECRET)
+  const publicKey = keyPair.publicKey()
+  const { server, StellarSdk } = getStellarServer(req.url)
+  const verified = speakeasy.totp.verify({
+    secret,
+    token: req.query.code,
+    encoding: 'base32'
+  });
 
-  if (token && req.query.code) {
-    try {
-      const tokenData = getJwt(token, secrets.JWT_SECRET);
-      const verified = speakeasy.totp.verify({
-        secret: tokenData.sub,
-        token: req.query.code,
-        encoding: 'base32'
-      });
+  console.log(
+    crypto.randomBytes(64).toString('hex')
+  );
 
-      if (verified) {
-        const keyPair = generateKeyPair(req.url, tokenData.sub + secrets.STR_SECRET)
-        const publicKey = keyPair.publicKey()
-
-        res.json({
-          publicKey
-        })
-      }
-
-      else {
-        next({
-          status: 400,
-          message: 'Invalid 2fa code'
-        })
-      }
-    } catch(err) {
-      next(err)
-    }
-  }
-
-  else if (req.query.id) {
-    const secret = encrypt(req.query.id)
-
-    return QRCode.toDataURL(`otpauth://totp/StellarAuth?secret=${secret}&algorithm=SHA512`)
-    .then((qrCode) => ({
-      token: setJwt({
+  server.loadAccount(publicKey)
+  .then(() => {
+    if (verified) {
+      const token = setJwt({
         sub: secret,
         iat: parseInt(moment().format('X')),
-        exp: parseInt(moment().add(1, 'day').format('X'))
-      }, secrets.JWT_SECRET),
-      secret,
-      qrCode
-    }))
-    .then((response) => res.json(response))
-    .catch((err) => next(err));
-  }
+        exp: parseInt(moment().add(1, 'hour').format('X'))
+      }, secrets.JWT_SECRET);
 
-  else
-    next()
+      return { token }
+    }
+
+    throw {
+      status: 400,
+      message: 'Invalid 2fa code'
+    }
+  })
+  .catch(StellarSdk.NotFoundError, () => {
+    if (verified)
+      return { publicKey }
+
+    else
+      return QRCode.toDataURL(`otpauth://totp/StellarAuth?secret=${secret}&algorithm=SHA512`)
+      .then((qrCode) => ({
+        secret,
+        qrCode
+      }))
+  })
+  .then((response) => res.json(response))
+  .catch((err) => next(err));
 }
